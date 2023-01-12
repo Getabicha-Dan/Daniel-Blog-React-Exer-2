c3d02bf311b9f8c48543ce806566750650bbd7df
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2023-01-12 13:15:56 +0200

    Dani blog exer Updated

--- daniel-blog/src/App.js
+++ daniel-blog/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const titel = "Well come to the Dani page";
+  const likes = 50;
+  const link = "https://www.mekina.net/";
+  const otherLink = "https://www.ethiopianairlines.com/";
+  const advert = "African Pride";
+  const num = 100;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="content">
+        <Home/>
+        <h1>{titel}</h1>
+        <p> Liked {likes} times per day</p>
+        <a href={link}>Car Page</a>
+        <br />
+        <a href={otherLink}>Buy here</a>
+        <h2>We are {advert} for the last {num} years!</h2>
+
+      </div>
+      
     </div>
   );
 }
