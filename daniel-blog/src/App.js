2ef705a6e761f698be5b29e83db0e39567f99965
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2023-01-16 13:28:24 +0200

    upadted till by Using state method

--- daniel-blog/src/App.js
+++ daniel-blog/src/App.js
@@ -1,27 +1,14 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const titel = "Well come to the Dani page";
-  const likes = 50;
-  const link = "https://www.mekina.net/";
-  const otherLink = "https://www.ethiopianairlines.com/";
-  const advert = "African Pride";
-  const num = 100;
+  
 
   return (
     <div className="App">
       <Navbar/>
       <div className="content">
         <Home/>
-        <h1>{titel}</h1>
-        <p> Liked {likes} times per day</p>
-        <a href={link}>Car Page</a>
-        <br />
-        <a href={otherLink}>Buy here</a>
-        <h2>We are {advert} for the last {num} years!</h2>
-
       </div>
       
     </div>
--- daniel-blog/src/Home.js
+++ daniel-blog/src/Home.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 const Home = () => {
+
+    const [name,setName]= useState('Rina');
+    const[age,setAge]= useState(7);
+    const[kitchenstaff,setKitchenstaff] = useState('Dish Washer');
+
+    const handelClick =()=>{
+        setName('Barok');
+        setAge(2);
+        setKitchenstaff('Microwave');
+
+        
+    }
+    
     return (
         <div className="Home">
             <h2>Home Page</h2>
-        </div>
+            <p>{name} is {age}</p>
+            <p>first {kitchenstaff} to be installed.</p>
+            <button onClick={handelClick}>Click Me</button>
 
+       </div>
     );
 }
  
